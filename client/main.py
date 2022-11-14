42476a4fa70ca7abcc650b0bcd225051681b6d69
n0x1s <ilyasselfatimy9@gmail.com>
2022-11-14 19:43:17 +0100

    just buy mx keys :(

--- client/main.py
+++ client/main.py
@@ -4,78 +4,84 @@ import sys
 import os
 
 PRE_HOTKEYS = {
-	'<ctrl>+<alt>+t': lambda self: self.toggel_suppress
+    '<ctrl>+<alt>+t': lambda self: self.toggel_suppress
 }
 
 def lazy_debug(event, key):
-	print(f'{hotkeys.is_alive()} -> {listener._suppress} -> {event} -> {key}')
+    print(f'{hotkeys.is_alive()} -> {listener._suppress} -> {event} -> {key}')
 
 class Client:
-	def __init__(self, suppress, host, port):
-		self.host = host
-		self.port = port
-		self.socket = socket.socket()
-		self.socket.connect((self.host, self.port))
-		self.suppress = suppress
-		self._init_listener()
-		self._init_hotkeys()
+    def __init__(self, suppress, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket()
+        self.socket.connect((self.host, self.port))
+        self.suppress = suppress
+        self._init_listener()
+        self._init_hotkeys()
 
-	def _init_listener(self):
-		self.listener = keyboard.Listener(suppress=self.suppress,
-		on_press= lambda key: self.on_press(key, self.socket),
-			on_release = lambda key: self.on_release(key, self.socket))
+    def _init_listener(self):
+        self.listener = keyboard.Listener(suppress=self.suppress,
+        on_press= lambda key: self.on_press(key, self.socket),
+            on_release = lambda key: self.on_release(key, self.socket))
 
-	def _init_hotkeys(self):
-		PRE_HOTKEYS = {
-			'<ctrl>+<alt>+t': self.toggel_suppress
-		}
-		self.hotkeys = keyboard.GlobalHotKeys(PRE_HOTKEYS)
+    def _init_hotkeys(self):
+        PRE_HOTKEYS = {
+            '<ctrl>+<alt>+t': self.toggel_suppress
+        }
+        self.hotkeys = keyboard.GlobalHotKeys(PRE_HOTKEYS)
 
-	def _start_client(self):
-		self.listener.start()
-		self.hotkeys.start()
-		self.listener.wait()
-		self.hotkeys.wait()
-		self.listener.join()
-		self.hotkeys.join()
+    def _start_client(self):
+        self.listener.start()
+        self.hotkeys.start()
+        self.listener.wait()
+        self.hotkeys.wait()
+        self.listener.join()
+        self.hotkeys.join()
 
-	def _stop_client(self):
-		self.listener.stop()
-		self.hotkeys.stop()
+    def _stop_client(self):
+        self.listener.stop()
+        self.hotkeys.stop()
 
-	def toggel_suppress(self):
-		print('toggel supppress')
-		if os.name != 'nt':
-			self._stop_client()
-			self._init_listener()
-			self._init_hotkeys()
-			self.suppress = not self.suppress
-			self._start_client()
-		else:
-			self.suppress = not self.suppress
-			self.listener._suppress = self.suppress
+    def toggel_suppress(self):
+        print('toggel supppress')
+        if os.name != 'nt':
+            self._stop_client()
+            self._init_listener()
+            self._init_hotkeys()
+            self.suppress = not self.suppress
+            self._start_client()
+        else:
+            self.suppress = not self.suppress
+            self.listener._suppress = self.suppress
 
-	@staticmethod
-	def on_press(key, socket_connection):
-		key = [1:-1:]
-		event = 'press'
-		message = f'{event}->{key}END;'
-		socket_connection.send(message.encode())
-		print(key)
+    @staticmethod
+    def on_press(key, socket_connection):
+        if hasattr(key, 'char'):
+            char = key.char
+        else:
+            char = str(key)
+        event = 'press'
+        message = f'{event}->{char}END;'
+        socket_connection.send(message.encode())
+        print(key)
 
-	@staticmethod
-	def on_release(key, socket_connection):
-		key = [1:-1:]
-		event = 'release'
-		message = f'{event}->{key}END;'
-		socket_connection.send(message.encode())
-		print(key)
+    @staticmethod
+    def on_release(key, socket_connection):
+        if hasattr(key, 'char'):
+            char = key.char
+        else:
+            char = str(key)
+        event = 'release'
+        message = f'{event}->{char}END;'
+        socket_connection.send(message.encode())
+        print(key)
 
 host = 'windows'
 if len(sys.argv) == 2:
-	host = sys.argv[1]
+    host = sys.argv[1]
 
-client = Client(True, 'windows', 5000)
+client = Client(True, host, 5000)
 client._start_client()
 
 # def toggel_suppress():
