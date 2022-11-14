de95ddfe59ed06034e9352b099703181b5bf0e2d
n0x1s <n0x1s0x01@gmail.com>
2022-11-14 16:58:44 +0100

    420

--- server/main.py
+++ server/main.py
@@ -12,8 +12,8 @@ class ClientConnection:
 		return str(self)
 
 class Server:
-	def __init__(self, hostname, port):
-		self.hostname = hostname
+	def __init__(self, host, port):
+		self.host = host
 		self.port = port
 		self.socket = socket.socket()
 
@@ -32,29 +32,33 @@ class Server:
 				print(data)
 
 def server_program():
+	server = Server('windows', 5000)
+	server.listen()
+	client = server.accept_client()
+	server.receive_commands(client)
     # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        # data = input(' -> ')
-        # conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
+    # host = socket.gethostname()
+    # port = 5000  # initiate port no above 1024
+	#
+    # server_socket = socket.socket()  # get instance
+    # # look closely. The bind() function takes tuple as argument
+    # server_socket.bind((host, port))  # bind host address and port together
+	#
+    # # configure how many client the server can listen simultaneously
+    # server_socket.listen(2)
+    # conn, address = server_socket.accept()  # accept new connection
+    # print("Connection from: " + str(address))
+    # while True:
+    #     # receive data stream. it won't accept data packet greater than 1024 bytes
+    #     data = conn.recv(1024).decode()
+    #     if not data:
+    #         # if data is not received break
+    #         break
+    #     print("from connected user: " + str(data))
+    #     # data = input(' -> ')
+    #     # conn.send(data.encode())  # send data to the client
+	#
+    # conn.close()  # close the connection
 
 
 if __name__ == '__main__':
