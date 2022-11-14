b554c344b2fe53b3c8d76209b7f10cb60157a047
n0x1s <ilyasselfatimy9@gmail.com>
2022-11-14 16:23:50 +0100

    fixed some erros

--- main.py
+++ main.py
@@ -2,53 +2,54 @@ from pynput import keyboard
 import os
 
 PRE_HOTKEYS = {
-	'<ctrl>+<alt>+t': lambda self: self.toggel_suppress
+    '<ctrl>+<alt>+t': lambda self: self.toggel_suppress
 }
 
 def lazy_debug(event, key):
-	print(f'{hotkeys.is_alive()} -> {listener._suppress} -> {event} -> {key}')
+    print(f'{hotkeys.is_alive()} -> {listener._suppress} -> {event} -> {key}')
 
 class Client:
-	def __init__(self, suppress):
-		self.suppress = suppress
-		self.listener = self._init_listener()
+    def __init__(self, suppress):
+        self.suppress = suppress
+        self._init_listener()
+        self._init_hotkeys()
 
-	def _init_listener(self):
-		self.listener = keyboard.Listener(suppress=self.suppress, on_press=self.on_press, on_release=self.on_press)
+    def _init_listener(self):
+        self.listener = keyboard.Listener(suppress=self.suppress, on_press=self.on_press, on_release=self.on_press)
 
-	def _init_hotkeys(self):
-		PRE_HOTKEYS = {
-			'<ctrl>+<alt>+t': self.toggel_suppress
-		}
-		self.hotkeys = keyboard.GlobalHotKeys(PRE_HOTKEYS)
+    def _init_hotkeys(self):
+        PRE_HOTKEYS = {
+            '<ctrl>+<alt>+t': self.toggel_suppress
+        }
+        self.hotkeys = keyboard.GlobalHotKeys(PRE_HOTKEYS)
 
-	def _start_client(self):
-		self.listener.start()
-		self.hotkeys.start()
-		self.listener.wait()
-		self.hotkeys.wait()
-		self.listener.join()
-		self.hotkeys.join()
+    def _start_client(self):
+        self.listener.start()
+        self.hotkeys.start()
+        self.listener.wait()
+        self.hotkeys.wait()
+        self.listener.join()
+        self.hotkeys.join()
 
-	def _stop_client(self):
-		self.listener.stop()
-		self.hotkeys.stop()
+    def _stop_client(self):
+        self.listener.stop()
+        self.hotkeys.stop()
 
-	def toggel_suppress(self):
-		print('toggel supppress')
-		self._stop_client()
-		self._init_listener()
-		self._init_hotkeys()
-		self.suppress = not self.suppress
-		self._start_client()
+    def toggel_suppress(self):
+        print('toggel supppress')
+        self._stop_client()
+        self._init_listener()
+        self._init_hotkeys()
+        self.suppress = not self.suppress
+        self._start_client()
 
-	@staticmethod
-	def on_press(key):
-		print(key)
+    @staticmethod
+    def on_press(key):
+        print(key)
 
-	@staticmethod
-	def on_release(key):
-		print(key)
+    @staticmethod
+    def on_release(key):
+        print(key)
 
 
 client = Client(True)
